0b9aaa8cee69d5fbf5d180a06b1a291a8f66f52a
avivkal <34456520+avivkal@users.noreply.github.com>
2020-11-07 22:28:33 +0200

    Update githubScript.py

--- githubScript.py
+++ githubScript.py
@@ -177,7 +177,7 @@ def wholeRami():
         time.sleep(1)
         
 if __name__ == '__main__':
-    print('omg its actually working')
+    print('omg its actually ')
     print(getserial())
     thread1 = threading.Thread(target=ask).start()
     thread2 = threading.Thread(target=wholeRami).start()
