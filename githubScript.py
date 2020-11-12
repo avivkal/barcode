fbe0c0f56a93247fcd87482e778dd9a940522974
avivkal <34456520+avivkal@users.noreply.github.com>
2020-11-12 22:00:58 +0200

    Update githubScript.py

--- githubScript.py
+++ githubScript.py
@@ -26,8 +26,7 @@ customerName="Aviv"
 chrome_options = Options()
 #chrome_options.add_argument('--headless')
 #chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--start-maximized')
+
 
 def getserial():
   # Extract serial from cpuinfo file
