823cf4fdf04701588bb55421666a207b1f2061b5
avivkal <34456520+avivkal@users.noreply.github.com>
2020-12-09 22:25:18 +0200

    Update requestsScript.py

--- requestsScript.py
+++ requestsScript.py
@@ -375,7 +375,9 @@ def addProductToDB(barcode,added):
     response9 = requests.post('https://www.rami-levy.co.il/api/catalog', headers=headers9, data=str(dataDict))
     ramiPrice = json.loads(response9.text).get('data')[0].get('price').get('price')
     response10 = requests.get('https://kalmanscan.herokuapp.com/products/getDataPartial/' + barcode)
-    print(response10.content)
+    image = json.loads(response10.text).get('image')
+    name = json.loads(response10.text).get('name')
+    print(name)
     productsRef.insert_one({"email": currentUser.get('email'),"selection":currentUser.get('selection'),"barcode":barcode,"creationDate": datetime.datetime.now(),"added":added, "shufersalPrice": shufersalPrice, "ramiLevyPrice":ramiPrice})
 
 def ask():
