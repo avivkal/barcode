9c37ad1281f00f959310502b87523caaf5588099
avivkal <34456520+avivkal@users.noreply.github.com>
2020-12-19 18:13:08 +0200

    Update requestsScript.py

--- requestsScript.py
+++ requestsScript.py
@@ -18,9 +18,9 @@ userPassword = ""
 currentPrice = 0
 currentUser= ""
 
-project_folder = os.path.expanduser('~/home/pi/real/barcode/')
-env_path = Path('/home/pi/real/barcode/.')
-load_dotenv(os.path.join(project_folder, '.env'))
+BASEDIR = os.path.abspath(os.path.dirname('/home/pi/real/barcode/'))
+load_dotenv(os.path.join(BASEDIR, '.env'))
+
 
 class mydict(dict):
     def __str__(self):
