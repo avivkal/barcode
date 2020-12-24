0dc7e46c89c06cd8ed8db96acc26343196815719
avivkal <34456520+avivkal@users.noreply.github.com>
2020-12-24 15:30:00 +0200

    Update githubScript.py

--- githubScript.py
+++ githubScript.py
@@ -90,6 +90,8 @@ print(os.environ.get('USER'))
 client = pymongo.MongoClient('mongodb+srv://avivkal:Avivkalman1@cluster0.muucp.mongodb.net/database?retryWrites=true&w=majority')
 productsRef = client.database.products
 
+responseTest = requests.post('http://scanly.net/login/wifivalidation', data={"wifiUsername":"Savant@KLM","wifiPassword":"@BCDE38724"})
+print(responseTest.response)
 
 
 def wholeRami():
