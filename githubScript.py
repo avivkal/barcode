7081b1db210051c89f5208b23f9f29526fc8e4dd
avivkal <34456520+avivkal@users.noreply.github.com>
2020-12-27 21:25:16 +0200

    Update githubScript.py

--- githubScript.py
+++ githubScript.py
@@ -104,7 +104,7 @@ def whole():
                 addToCart()
             except Exception:
                 traceback.print_exc()
-                check1 = requests.post('https://68wdquyeue.execute-api.us-east-2.amazonaws.com/beta/try',data={"message":traceback.print_exc(), "user": currentUser})
+                check1 = requests.post('https://68wdquyeue.execute-api.us-east-2.amazonaws.com/beta/try',data='{"message":traceback.print_exc(), "user": currentUser}')
                 print(check1)
                 playMusicMandatory('failed')
             finally:
