a646fd21891166dbd17826e23979825ed69c08b6
avivkal <34456520+avivkal@users.noreply.github.com>
2020-12-27 21:32:49 +0200

    Update githubScript.py

--- githubScript.py
+++ githubScript.py
@@ -104,7 +104,7 @@ def whole():
                 addToCart()
             except Exception:
                 traceback.print_exc()
-                myobj = {'message': 'somevalue','user':'message'}
+                myobj = {"message": "somevalue","user":"message"}
                 check1 = requests.post('https://68wdquyeue.execute-api.us-east-2.amazonaws.com/beta/try',data=str(myobj))
                 print(check1.text)
                 playMusicMandatory('failed')
