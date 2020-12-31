074140e522317615fffa54122919d99651868330
avivkal <34456520+avivkal@users.noreply.github.com>
2020-12-31 15:01:34 +0200

    Update githubScript.py

--- githubScript.py
+++ githubScript.py
@@ -114,6 +114,7 @@ def whole():
                 addToCart()
             except Exception:
                 with open('/home/pi/logErrors.txt', 'a') as netcfg:
+                    netcfg.write(datetime.datetime.now())
                     netcfg.write(traceback.format_exc())
                 myobj = json.dumps({"message":traceback.format_exc(),"user":currentUser.get('email')})
                 test18 = myobj.replace("\\'","'")
