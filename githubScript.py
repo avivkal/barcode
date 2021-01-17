2f0fa3d22a95e48f7700149a8fdd555b4327aee7
avivkal <34456520+avivkal@users.noreply.github.com>
2021-01-18 00:06:37 +0200

    Update githubScript.py

--- githubScript.py
+++ githubScript.py
@@ -39,7 +39,7 @@ def getserial():
     return cpuserial
 
 print(getserial())
-url = pyqrcode.create(getserial(),version=1)
+url = pyqrcode.create(getserial(),version=3)
 url.svg('uca-url.svg', scale=8)
 url.eps('uca-url.eps', scale=2)
 print(url.terminal(quiet_zone=1))
