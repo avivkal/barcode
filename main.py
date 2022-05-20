ff12affe5da1e88f701f83efac52a4c256699d11
Aviv Kalmanson <avivkalmanson@gmail.com>
2022-05-21 00:40:41 +0300

    add main

---
+++ main.py
@@ -0,0 +1,125 @@
+import datetime
+import json
+import os
+import socket
+import threading
+import time
+import traceback
+import pygame
+import requests
+
+token = ""
+array = []
+
+
+def getserial():
+    # Extract serial from cpuinfo file
+    cpuserial = "0000000000000000"
+    try:
+        f = open('/proc/cpuinfo', 'r')
+        for line in f:
+            if line[0:6] == 'Serial':
+                cpuserial = line[10:26]
+        f.close()
+    except:
+        cpuserial = "ERROR000000000"
+
+    return cpuserial
+
+
+def playMusicMandatory(fileName):
+    try:
+        pygame.mixer.init()
+        pygame.mixer.music.load("/home/pi/real/barcode/" + fileName + ".mp3")
+        pygame.mixer.music.play()
+    except Exception:
+        logError()
+
+
+def logError():
+    playMusicMandatory('error')
+    time.sleep(5)
+    try:
+        with open('/home/pi/logErrors.txt', 'a') as netcfg:
+            netcfg.write(str(datetime.datetime.now()))
+            netcfg.write(str(traceback.format_exc()))
+    except:
+        print('Problem writing to error file')
+    try:
+        myobj = json.dumps(
+            {"message": traceback.format_exc(), "user": getserial()})
+        log_aws_response = requests.post(
+            'https://68wdquyeue.execute-api.us-east-2.amazonaws.com/beta/try', data=myobj)
+        print(log_aws_response.text)
+    except:
+        print('No wifi so couldnt log')
+
+
+def internet(host="8.8.8.8", port=53, timeout=3):
+    """
+    Host: 8.8.8.8 (google-public-dns-a.google.com)
+    OpenPort: 53/tcp
+    Service: domain (DNS/TCP)
+    """
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except socket.error as ex:
+        print(ex)
+        return False
+
+
+def main_loop():
+    while True:
+        if len(array) > 0:
+            try:
+                addToCart()
+            except Exception:
+                logError()
+            finally:
+                array.pop(0)
+        time.sleep(1)
+
+
+def addToCart():
+    barcode = array[0]
+    add_product_response = requests.post(
+        'https://scanly.net/api/products', data={"barcode": barcode}
+    )
+
+    if add_product_response == 'true':
+        playMusicMandatory('added')
+    else:
+        playMusicMandatory('addedList')
+
+
+def ask():
+    barcode = input('enter barcode')
+    print('your original barcode is' + barcode)
+    array.append(barcode)
+    print(array)
+    ask()
+
+
+if __name__ == '__main__':
+    try:
+        print('No internet currently')
+        playMusicMandatory('noInternet')
+
+        credentials = open("cred.txt", "r")
+        while(credentials.read() == ""):
+            print("credentials:", credentials.read())
+            credentials = open("cred.txt", "r")
+            time.sleep(1)
+
+        print('Connected Succesfully')
+        playMusicMandatory('wifiConnected')
+
+        token = credentials.read()
+        ask_thread = threading.Thread(target=ask).start()
+        main_loop_thread = threading.Thread(target=main_loop).start()
+    except Exception:
+        logError()
+        time.sleep(5)
+        os.system("sudo reboot")
